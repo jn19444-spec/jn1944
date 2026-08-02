4ad40ce0128de6290d557c675de9ec2d8e518f84
jn19444-spec <jn19444@gmail.com>
2026-08-02 18:49:52 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/jn1944/', // 👈 이 줄을 꼭 추가해 주어야 합니다!
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
